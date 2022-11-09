9b4d5a9317679fceb247af213283b10708f039b6
OsahonOjo <osahonobor@yahoo.com>
2022-11-08 23:27:12 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+/* libraries */
+import React from 'react';
+
+/* components */
+import SideNavigationMenu from "./SideNavigationMenu.js";
+
+/* style */
+import '../styles/App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
